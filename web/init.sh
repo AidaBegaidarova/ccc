1de141354e103a2ace4488db3b6ff0e2dbfb6bee
Mark Gerasimov <728239@gmail.com>
2016-03-16 21:08:41 +0300

    -//-

---
+++ web/init.sh
@@ -0,0 +1,4 @@
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/gunicorn restart
