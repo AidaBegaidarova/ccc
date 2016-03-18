6c5ec06a837c8c2c093226c2ce3d2ec45a9a3747
2 <1>
2016-03-18 21:20:35 +0000

    some corrections

--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r"^$", views.test, name='test'),
-    url(r"^login$", views.test, name='login'),
-    url(r"^signup$", views.test, name='signup'),
-    url(r"^ask$", views.test, name='ask'),
-    url(r"^popular$", views.test, name='popular'),
-    url(r"^question/(\d+)$", views.test, name='question'),
+    url(r"^login/$", views.test, name='login'),
+    url(r"^signup/$", views.test, name='signup'),
+    url(r"^new/$", views.test, name='new'),
+    url(r"^ask/.*$", views.test, name='ask'),
+    url(r"^popular/$", views.test, name='popular'),
+    url(r"^question/(\d+)/$", views.test, name='question'),
 ]
