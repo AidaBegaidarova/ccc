6f405dfd2649b6badf91f41dd568ec5135101ce8
Mark Gerasimov <728239@gmail.com>
2016-03-24 21:34:36 +0300

    some improvement for non-ex pages

--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.decorators.http import require_GET
 from qa.models import Question
 
@@ -18,22 +18,24 @@ def question(request, q_id):
     q = get_object_or_404(Question, id=q_id)
     return render(request, 'qa/question.html',
                   {'question': q,
-                   'answers': q.answer_set.all()[:]})
+                   'answers': q.answer_set.all()})
 
 
 @require_GET
-def index(request, *args, **kwargs):  # TODO
+def index(request, *args, **kwargs):
     questions = Question.objects.all()
     num = request.GET.get('page', 1)
     limit = request.GET.get('limit', 2)
     paginator = Paginator(questions, limit)
+    if len(paginator.page_range) < int(num) or int(num) < 1:
+        raise Http404()
     paginator.baseurl = '/?page='
     page = paginator.page(num)
     return render(request, 'qa/page.html',
-        {'posts': page.object_list,
-            'paginator': paginator,
-            'page': page,
-            'question_url': '/question/'})
+                  {'posts': page.object_list,
+                   'paginator': paginator,
+                   'page': page,
+                   'question_url': '/question/'})
 
 
 @require_GET
