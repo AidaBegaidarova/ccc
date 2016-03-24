c2a4edf7fc679b786302d6947ca22a7a0c324196
Mark Gerasimov <728239@gmail.com>
2016-03-24 21:50:03 +0300

    task 2.5 implemented

--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -24,21 +24,28 @@ def question(request, q_id):
 @require_GET
 def index(request, *args, **kwargs):
     questions = Question.objects.all()
+    questions = questions.order_by('-added_at')
+    return pagination(request, questions, '/?page=')
+
+
+@require_GET
+def popular(request, *args, **kwargs):
+    questions = Question.objects.all()
+    questions = questions.order_by('-rating')
+    return pagination(request, questions, '/popular/?page=')
+
+
+# TODO: move this helper function somewhere
+def pagination(request, questions, url):
     num = request.GET.get('page', 1)
-    limit = request.GET.get('limit', 2)
+    limit = request.GET.get('limit', 10)
     paginator = Paginator(questions, limit)
     if len(paginator.page_range) < int(num) or int(num) < 1:
         raise Http404()
-    paginator.baseurl = '/?page='
+    paginator.baseurl = url
     page = paginator.page(num)
     return render(request, 'qa/page.html',
                   {'posts': page.object_list,
                    'paginator': paginator,
                    'page': page,
                    'question_url': '/question/'})
-
-
-@require_GET
-def popular(request, *args, **kwargs):  # TODO
-    resp = 'TODO: show popular here'
-    return HttpResponse(resp)
