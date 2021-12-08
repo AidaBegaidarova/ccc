566f1f270678e8e05c642e3cbc2ecdb1045dfa40
AidaBegaidarova <92906571+AidaBegaidarova@users.noreply.github.com>
2021-12-08 22:49:35 +0600

    Update init.sh

--- web/init.sh
+++ web/init.sh
@@ -2,14 +2,14 @@
 curDir=$(pwd)
 
 	
-	sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-	sudo rm -rf /etc/nginx/sites-enabled/default
+	sudo ln -sf /home/box/web/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+	sudo rm -rf /home/box/web/web/etc/nginx/sites-enabled/default
 	sudo /etc/init.d/nginx restart
 
-	sudo ln -sf ~/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-	sudo /etc/init.d/gunicorn restart
+	sudo ln -sf /home/box/web/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+	sudo /home/box/web/web/etc/init.d/gunicorn restart
 
-	cd ~/web/ask/ask
+	cd /home/box/web/web/ask/ask
 	gunicorn -b 127.0.0.1:8000 wsgi
 
 
