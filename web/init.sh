0f5ce9519e5d7211c4849386464cd90d38b54fca
AidaBegaidarova <92906571+AidaBegaidarova@users.noreply.github.com>
2021-12-08 22:40:06 +0600

    Update init.sh

--- web/init.sh
+++ web/init.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 curDir=$(pwd)
-homeEnv='/home/user/StepicWebTech/web'
-testEnv='/home/box/web'
 
-
-if [ $curDir == $homeEnv ]; then
-        echo 'home env'
-
-	sudo ln -s /home/user/StepicWebTech/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-	sudo /etc/init.d/nginx restart
-
-	cd ask
-	sudo gunicorn -b 127.0.0.1:8000 ask.wsgi
-elif [ $curDir == $testEnv ]; then
-        echo 'test env'
-
-	./createDB.sh
 	
 	sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 	sudo rm -rf /etc/nginx/sites-enabled/default
@@ -27,8 +12,5 @@ elif [ $curDir == $testEnv ]; then
 	cd ~/web/ask/ask
 	gunicorn -b 127.0.0.1:8000 wsgi
 
-else
-        echo 'unknown env!'
-	exit
-fi
+
 
