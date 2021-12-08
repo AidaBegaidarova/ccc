6cfda17c1e0efad1d071162ed3ba8e2a8872152d
AidaBegaidarova <92906571+AidaBegaidarova@users.noreply.github.com>
2021-12-08 22:58:47 +0600

    Update wsgi.py

--- web/ask/ask/wsgi.py
+++ web/ask/ask/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+cd /home/box/web/web/ask/
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
 from django.core.wsgi import get_wsgi_application
